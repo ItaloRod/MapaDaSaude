d9df057946b74e313a341eac860022302c06867b
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2020-01-10 13:01:01 -0300

    Implementação indicadores - teste homologa

--- src/protected/application/themes/Saude/views/indicadores/instituicoes-saude.php
+++ src/protected/application/themes/Saude/views/indicadores/instituicoes-saude.php
@@ -2,6 +2,6 @@
         src="http://10.17.0.171:3000/public/dashboard/42cfcdb7-93f6-44df-9d67-a075a1ae185f"
         frameborder="0"
         width="100%"
-        height="1002px"
+        height="1000px"
         allowtransparency
 ></iframe>
